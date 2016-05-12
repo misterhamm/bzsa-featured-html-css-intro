c71b3d515c0a6e3fcfc2d7a06014731556260054
Chris Hamm <chamm@bizstream.com>
2016-05-12 15:43:06 -0400

    merge in stephanie's changes to homework

---
+++ js/homework.js
@@ -0,0 +1,13 @@
+﻿
+$( document ).ready( function () {
+
+    $(".sidebar li a").click(function () {
+        $(".sidebar li a").removeClass("active");
+        $(this).toggleClass("active");
+
+        $(".main-content .class-assignment").removeClass("active");
+        var className = $(this).parent().attr('class');
+        $(".main-content #" + className).toggleClass("active");
+    });
+
+});
